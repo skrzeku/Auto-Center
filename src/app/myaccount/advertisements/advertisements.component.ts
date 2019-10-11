673c6b7f604175a1caf95b4a4f846c567b4389b4
skrzeku <pawelskrzeszewski92@gmail.com>
2019-10-11 19:01:27 +0200

    myaccount changes

--- src/app/myaccount/advertisements/advertisements.component.ts
+++ src/app/myaccount/advertisements/advertisements.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {CarsService} from '../../core-module/services/cars.service';
+import {Car} from '../../core-module/models/car.model';
+import {Observable} from 'rxjs';
 
 @Component({
   selector: 'app-advertisements',
@@ -7,13 +10,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AdvertisementsComponent implements OnInit {
 
-  amountOfCurrent: string = '';
-  amountOfRecent: string = '(2)';
+  amountOfCurrent: number = 2;
+  amountOfRecent: number = 0;
+  booli: boolean = true;
+  currentBoolean: boolean = true;
   category: string[] = ['Osobowe', 'Ciężarowe', 'Motocykle', 'Części'];
+  amount: string[] = ['10', '20', '50'];
+  cars: Car[];
 
-  constructor() { }
+  constructor( private carservice: CarsService) { }
 
   ngOnInit() {
+    this.carservice.getCars().subscribe((cars) => {
+      this.cars = cars;
+      this.amountOfCurrent = this.cars.length;
+    });
+
+  }
+  changevoid(str: string) {
+    if (str === 'curr') {
+      this.currentBoolean = true;
+    }
+    if (str === 'rec') {
+      this.currentBoolean = false;
+    }
+    else return;
+  }
+  gotoDetails(car: Car) {
+    this.carservice.goToCarDetails(car);
+
+  }
+  sortIt() {
+    this.booli = !this.booli;
   }
 
 }
